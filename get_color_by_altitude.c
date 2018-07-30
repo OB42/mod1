17613189c0f4135d137ac09a02d9d6badca3df01
Otmane BENAZZOU <obenazzo@e2r12p13.42.fr>
2018-07-30 19:04:51 +0200

    adding more colors

--- get_color_by_altitude.c
+++ get_color_by_altitude.c
@@ -15,17 +15,30 @@
 
 int		get_color_by_altitude(int n, float max)
 {
-	static int colors[7] = {
-		0x33cc33,
-		0x009933,
-		0xcccc00,
-		0xcc9900,
-		0x996600,
-		0xe6e6e6,
-		0xffffff
+	static int colors[20] = {
+		0x04AF17,
+		0x16A614,
+		0x3C9610,
+		0x61860B,
+		0x747E09,
+		0x867606,
+		0x996E04,
+		0xAC6602,
+		0xBF5E00,
+		0xC56E19,
+		0xCB7E33,
+		0xD28E4C,
+		0xD89E66,
+		0xDFAE7F,
+		0xE5BE99,
+		0xEBCEB2,
+		0xF2DECC,
+		0xF8EEE5,
+		0xFFFFEE,
+		0xFFFFFF
 	};
 
-	return (colors[n <= 0 ? 0 : (int)floor(n / max * 6.99)]);
+	return (colors[n <= 0 ? 0 : (int)floor(n / max * 19.99)]);
 }
 
 float		get_max_elev(t_stuffs *stuffs)
