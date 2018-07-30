8b49f200cf7323a45181393487424ff4cf31f8c6
Otmane BENAZZOU <obenazzo@e2r12p13.42.fr>
2018-07-30 17:19:20 +0200

    fixing "holes" bug

--- engine.c
+++ engine.c
@@ -1,3 +1,5 @@
+#include <unistd.h>
+
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
@@ -19,11 +21,12 @@ void		malloc_bigmap(t_stuffs *s)
 	int j;
 	int z;
 
-	s->bigmap = pr_malloc(sizeof(t_p2d*) * s->size_x * s->linelen);
+	s->bigmap = pr_malloc(sizeof(t_p2d*) * (s->size_x * s->linelen + 1));
+
 	j = 0;
 	while (j < (s->size_x * s->linelen))
 	{
-		s->bigmap[j] = pr_malloc(sizeof(t_p2d) * s->size_y * s->linelen);
+		s->bigmap[j] = pr_malloc(sizeof(t_p2d) * (s->size_y * s->linelen + 1));
 		z = 0;
 		while (z < s->size_y * s->linelen)
 		{
@@ -34,23 +37,6 @@ void		malloc_bigmap(t_stuffs *s)
 	}
 }
 
-void		bigdot(t_p2d pt, int color, t_stuffs *s)
-{
-
-	mlx_pixel_put(s->co, s->win, pt.x, pt.y, color);
-
-	mlx_pixel_put(s->co, s->win, pt.x+1, pt.y, color);
-	mlx_pixel_put(s->co, s->win, pt.x-1, pt.y, color);
-	mlx_pixel_put(s->co, s->win, pt.x, pt.y+1, color);
-	mlx_pixel_put(s->co, s->win, pt.x, pt.y-1, color);
-
-	//put_pixel_in_img(s, dpoint.x, dpoint.y, color);
-	//put_pixel_in_img(s, dpoint.x, dpoint.y, color);
-	//put_pixel_in_img(s, dpoint.x, dpoint.y, color);
-	//put_pixel_in_img(s, dpoint.x, dpoint.y, color);
-	//put_pixel_in_img(s, dpoint.x, dpoint.y, color);
-
-}
 
 void             line(t_p2d p1, t_p2d p2, t_stuffs *s, int color)
 {
@@ -60,8 +46,6 @@ void             line(t_p2d p1, t_p2d p2, t_stuffs *s, int color)
 	init_line_stuffs(&val, p1, p2);
 	while (1)
 	{
-		//put_pixel_in_img(s, p1.x, p1.y, color);
-
 		mlx_pixel_put(s->co, s->win, p1.x, p1.y, color);
 		if (p1.x == p2.x && p1.y == p2.y)
 			break ;
@@ -121,17 +105,6 @@ void             special_line(t_p2d p1, t_p2d p2, t_stuffs *s, t_coords co)
 			else
 				dpoint.y -=  abs(p2.elev) ;
 		}
-
-		/*
-		   midpoint = (t_p2d){
-		   .x = (prevpoint.x + p1.x) /2,
-		   .y = (prevpoint.y + p1.y) /2,
-		   .elev = abs(dpoint.y - p1.y)
-		   };
-		   */
-
-
-
 		int a;
 		int b;
 
@@ -141,7 +114,6 @@ void             special_line(t_p2d p1, t_p2d p2, t_stuffs *s, t_coords co)
 		//int posinow = whereami * (float)line_len;
 		if (1)
 		{
-
 			prevcoords = (t_p2d){.x = a, .y = b};
 			if (co.x1 == co.x2)
 			{
@@ -153,43 +125,17 @@ void             special_line(t_p2d p1, t_p2d p2, t_stuffs *s, t_coords co)
 				b = (co.y1 - 1) * line_len;
 				a = ((co.x1 - 1) * line_len) + whereami * s->linelen;
 			}
-
-
-
 			if (co.x1 < 2)
 			{
-				printf("setting big map [%d][%d].elev = %d\n",a,b, dpoint.y - p1.y);
+				//printf("setting big map [%d][%d].elev = %d\n",a,b, dpoint.y - p1.y);
 			}
 			s->bigmap[a][b] = dpoint;
 			s->bigmap[a][b].elev = p1.y - dpoint.y ;//abs(dpoint.y - p1.y);
-
-			/*
-			   if (prevcoords.x != 0)
-			   {
-			   s->bigmap[(a + prevcoords.x) / 2][(b + prevcoords.y) /2] = midpoint;
-			   }
-			   */
-
-			//int k;
-			//scanf("%d\n",&k);
-
 		}
 
 
 		float red = fto.y + ((fto.x - fto.y)) * whereami;
-		/*
-		//color related
-		if (coef != whereami)
-		elevnow += p1.elev;
-		else
-		elevnow += p2.elev;
-		*/
-		//printf("%d\t", elevnow);
-		//float prr = (float)1 * ((float)elevnow * coef) + (float)50;
 		color = get_color(0, red, 128 - (red/2));
-
-		//mlx_pixel_put(s->co, s->win, dpoint.x, dpoint.y, color);
-		//put_pixel_in_img(s, dpoint.x, dpoint.y, color);
 		if (p1.x == p2.x && p1.y == p2.y)
 			break ;
 		prevpoint = p1;
@@ -210,24 +156,12 @@ void             special_fine_line(t_p2d p1, t_p2d p2, t_stuffs *s, t_coords co)
 	p2.y += p2.elev;
 	init_line_stuffs(&val, p1, p2);
 	line_len = line_count(p1,p2);
-
-	//printf("line len =%d\n",line_len);
 	cpt = 0;
 	t_p2d dpoint;
-
-
-
-
-
-	//to remove
-	//co.x1++;
-
-
 	fto = set_fromto(s, p1, p2);
 	while (1)
 	{
 		float whereami = ((float)cpt/(float)line_len);
-
 		float coef = whereami;
 		float sinuscorrect = -(M_PI/2.0);
 		int elevnow = ((float)p2.elev - (float)p1.elev);
@@ -237,76 +171,38 @@ void             special_fine_line(t_p2d p1, t_p2d p2, t_stuffs *s, t_coords co)
 			coef = 1 - whereami;
 			sinuscorrect = (M_PI/2.0);
 		}
-		float sinelev =  sin(whereami * M_PI +  sinuscorrect)+1;
+		float sinelev = sin(whereami * M_PI +  sinuscorrect)+1;
 		dpoint = p1;
-		dpoint.y -=  fabs(sinelev * (elevnow/2));
+		dpoint.y -= fabs(sinelev * (elevnow/2));
 		if (p1.elev != 0 &&  p2.elev != 0)
 		{
-
 			if (p2.elev > p1.elev)
 				dpoint.y -=  abs(p1.elev) ;
 			else
 				dpoint.y -=  abs(p2.elev) ;
 		}
 
-
 		float red = fto.y + ((fto.x - fto.y)) * whereami;
-		/*
-		//color related
-		if (coef != whereami)
-		elevnow += p1.elev;
-		else
-		elevnow += p2.elev;
-		*/
-		//printf("%d\t", elevnow);
-		//float prr = (float)1 * ((float)elevnow * coef) + (float)50;
-		//
-		//
-		//
-
 		int a;
 		int b;
-		if (1)
+		if (co.x1 == co.x2)
 		{
-
-			if (co.x1 == co.x2)
-			{
-				a = (co.x1 ) ;
-				b = ((co.y1 ) ) + whereami * s->linelen;
-			}
-			else
-			{
-				b = (co.y1 ) ;
-				a = ((co.x1 ) ) + whereami * s->linelen;
-			}
-
-
-
-			//printf("setting big map [%d][%d].elev = %d\n",a,b, dpoint.y - p1.y);
-			if (s->bigmap[a][b].x ==0)
-			{
+			a = (co.x1 ) ;
+			b = ((co.y1 ) ) + whereami * s->linelen;
+		}
+		else
+		{
+			b = (co.y1 ) ;
+			a = ((co.x1 ) ) + whereami * s->linelen;
+		}
+		if (s->bigmap[a][b].x ==0)
+		{
 			s->bigmap[a][b] = dpoint;
 			s->bigmap[a][b].elev = abs(dpoint.y - p1.y);
 			s->bigmap[a][b].color = -1;
-			}
-
-			//int k;
-			//scanf("%d\n",&k);
-
 		}
-
 		color = get_color(0, red, 128 - (red/2));
 
-		//mlx_pixel_put(s->co, s->win, dpoint.x, dpoint.y, color);
-		//
-		//bigdot(dpoint,color,s);
-
-		t_p2d cheat;
-		cheat = dpoint;
-		cheat.y +=5;
-		//cheat.elev -=5;
-	//	line(dpoint, cheat, s,color);
-		//put_pixel_in_img(s, dpoint.x, dpoint.y, color);
 		if (p1.x == p2.x && p1.y == p2.y)
 			break ;
 		next_pt_line(&val, &p1);
@@ -316,47 +212,44 @@ void             special_fine_line(t_p2d p1, t_p2d p2, t_stuffs *s, t_coords co)
 
 void	connect_fne_dots(t_stuffs *stuffs)
 {
-	int		e;
-	int		w;
-	float	max_elev;
-	int		color;
+		int		e;
+		int		w;
+		float	max_elev;
+		int		color;
 
-	max_elev = get_max_elev(stuffs);
-	e = 0;
-	ft_printf("Rendering fine dots...\n");
-	//while (e <= (stuffs->size_x * stuffs->linelen))
-	while (e <= (9 * stuffs->linelen))
-	{
-		w = 0;
-		//while (w <= (stuffs->size_y * stuffs->linelen))
-		while (w <= (9 * stuffs->linelen))
+		max_elev = get_max_elev(stuffs);
+		e = 0 * stuffs->linelen;
+		ft_printf("Rendering fine dots..\n");
+
+		//while (stuffs->bigmap[e])
+	 	int i = 0;
+		while (e < (stuffs->size_x * stuffs->linelen))
 		{
-
-			if ((stuffs->bigmap)[e][w].elev >= 0 )
+			w = 0 * stuffs->linelen;
+	//		while (stuffs->bigmap[e][w])
+			while (w < (stuffs->size_y * stuffs->linelen))
 			{
-				color = get_color_by_altitude(stuffs->bigmap[e][w].elev, max_elev);
-				//bigdot((stuffs->bigmap)[e][w],color,stuffs);
 
-		t_p2d cheat;
-		cheat = (stuffs->bigmap)[e][w];
-		cheat.y +=8;
-		//cheat.elev -=5;
-		line((stuffs->bigmap)[e][w], cheat, stuffs,color);
+				if ((stuffs->bigmap)[e][w].elev >= 0 )
+				{
+					color = get_color_by_altitude(stuffs->bigmap[e][w].elev, max_elev);
+					t_p2d cheat;
+					cheat = (stuffs->bigmap)[e][w];
+					cheat.y += 8;
+					line((stuffs->bigmap)[e][w], cheat, stuffs,color);
+				}
 
+				w++;
+				//w += stuffs->linelen;
 			}
-
-			w++;
-			//w += stuffs->linelen;
+			//e += stuffs->linelen;
+			e++;
 		}
-		//e += stuffs->linelen;
-		e++;
-	}
-	ft_printf("Done !\n\n");
-	//	connect_ufine_dots(stuffs);
+
+
+		ft_printf("Done %i!\n", stuffs->linelen);
+		//	connect_ufine_dots(stuffs);
 }
-
-
-
 
 void	connect_fine_dots(t_stuffs *stuffs)
 {
@@ -365,75 +258,48 @@ void	connect_fine_dots(t_stuffs *stuffs)
 
 	e = 0;
 	ft_printf("Rendering fine dots...\n");
-	//while (e <= (stuffs->size_x * stuffs->linelen))
-	while (e <= (8 * stuffs->linelen))
-	{
-		w = 0;
-		//while (w <= (stuffs->size_y * stuffs->linelen))
-		while (w <= (8 * stuffs->linelen))
+	while (e < ((stuffs->size_x - 1)* stuffs->linelen))
 		{
-//			printf("pour e=%d, e mod linelen = %d\n",e,e%stuffs->linelen);
-//
-
-			//int k;
-			//scanf("%d\n", &k);
-
-			if (w != stuffs->size_y * stuffs->linelen &&
-			1)//(e%stuffs->linelen) > 4	)
+			w = 0;
+	//		while (w < (9 * stuffs->linelen))
+			while ((w < (stuffs->size_y - 1) * stuffs->linelen))
 			{
-				if (
-						(stuffs->bigmap)[e][w].x != 0 &&
-						(stuffs->bigmap)[e][ w + stuffs->linelen - (w % stuffs->linelen)].x != 0 &&
-	(stuffs->bigmap)[e][w].color != -1 &&
-	(stuffs->bigmap)[e][ w + stuffs->linelen - (w % stuffs->linelen)].color != -1
-
-				   )
-
+				if (w != stuffs->size_y * stuffs->linelen)
 				{
-					special_fine_line((stuffs->bigmap)[e][w],
-							(stuffs->bigmap)[e][w + stuffs->linelen - (w %stuffs->linelen)],
-							stuffs,
-							(t_coords){.x1 = e, .y1 = w, .x2 = e, .y2 = w + stuffs->linelen - (w % stuffs->linelen) });
-
-					/*
-	line_col((stuffs->bigmap)[e][w], (stuffs->bigmap)[e]
-			[w + stuffs->linelen - (w % stuffs->linelen)], stuffs);
-			*/
+					if ((stuffs->bigmap)[e][w].x != 0 &&
+							(stuffs->bigmap)[e][ w + stuffs->linelen - (w % stuffs->linelen)].x > 0 &&
+							(stuffs->bigmap)[e][w].color != -1 &&
+							(stuffs->bigmap)[e][ w + stuffs->linelen - (w % stuffs->linelen)].color != -1)
+					{
+						special_fine_line((stuffs->bigmap)[e][w],
+								(stuffs->bigmap)[e][w + stuffs->linelen - (w %stuffs->linelen)],
+								stuffs,
+								(t_coords){.x1 = e, .y1 = w, .x2 = e, .y2 = w + stuffs->linelen - (w % stuffs->linelen) });
+					}
 				}
-			}
-
-
-			if (e != stuffs->size_x &&
-			1)//(w%stuffs->linelen) > 4)
-			{
-				if (
-						(stuffs->bigmap)[e][w].x != 0 &&
-						(stuffs->bigmap)[e + stuffs->linelen - (e % stuffs->linelen)][ w].x != 0 &&
-				(stuffs->bigmap)[e][w].color != -1 &&
-(stuffs->bigmap)[e + stuffs->linelen - (e % stuffs->linelen)][ w].color != -1
-
-				   )
+				if (e != stuffs->size_x)
 				{
-					special_fine_line((stuffs->bigmap)[e][w],
-							(stuffs->bigmap)[e + stuffs->linelen - (e % stuffs->linelen)][w ]
-							, stuffs,
-							(t_coords){.x1 = e, .y1 = w, .x2 = e + stuffs->linelen - (e % stuffs->linelen), .y2 = w });
-			//		line_col((stuffs->bigmap)[e][w], (stuffs->bigmap)[e + stuffs->linelen - (e % stuffs->linelen)][w], stuffs);
-				}
-			}
+					if (
+							(stuffs->bigmap)[e][w].x != 0 &&
+							(stuffs->bigmap)[e + stuffs->linelen - (e % stuffs->linelen)][ w].x != 0 &&
+					(stuffs->bigmap)[e][w].color != -1 &&
+	(stuffs->bigmap)[e + stuffs->linelen - (e % stuffs->linelen)][ w].color != -1
 
-			w++;
-			//w += stuffs->linelen;
+					   )
+					{
+						special_fine_line((stuffs->bigmap)[e][w],
+								(stuffs->bigmap)[e + stuffs->linelen - (e % stuffs->linelen)][w ]
+								, stuffs,
+								(t_coords){.x1 = e, .y1 = w, .x2 = e + stuffs->linelen - (e % stuffs->linelen), .y2 = w });
+					}
+				}
+				w++;
+			}
+			e++;
 		}
-		//e += stuffs->linelen;
-		e++;
-	}
 	ft_printf("Done !\n\n");
-	//	connect_ufine_dots(stuffs);
 	connect_fne_dots(stuffs);
 }
-
-
 
 void	connect_dots(t_stuffs *stuffs)
 {
@@ -456,7 +322,6 @@ void	connect_dots(t_stuffs *stuffs)
 			{
 				special_line((stuffs->map)[e][w], (stuffs->map)[e + 1][w], stuffs,
 						(t_coords){.x1 = e, .y1 = w, .x2 = e + 1, .y2 = w });
-
 			}
 			w++;
 		}
